da7cfee89909db1ec21a04c626aa87e9cac5d706
genkamioka <114816409+genkamioka@users.noreply.github.com>
2022-10-30 16:38:21 +0900

    2-1
    演算子練習

---
+++ PHPjob/2-1/index.php
@@ -0,0 +1,21 @@
+<?php
+$hallo = "hallo";
+$world = "world!";
+echo $hallo.$world;
+echo '<br>';
+echo 2 + 1;
+echo '<br>';
+echo 10 - 5;
+echo '<br>';
+echo 2 * 12;
+echo '<br>';
+echo 10 / 2;
+echo '<br>';
+echo 10 % 3 ;
+echo '<br>';
+$x++; 
+echo $x;
+echo '<br>';
+$x *= 5;
+echo $x;
+?>
